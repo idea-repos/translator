96c036f511ed9dd653219da3336cb94b7dfed96e
aman <er.amandeep04@gmail.com>
2022-04-28 18:35:57 +0530

    minor change

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def home():
 def translate():
 
     data = request.get_json()
-    if 'to' not in data or 'msg' not in data is None:
-        return jsonify({"status": 500,  "msg": "Please Enter Destination Language and Message in your API Resquest."})
-    translator = Translator(to_lang=data['to'])
+    if 'from' not in data or 'to' not in data or 'msg' not in data is None:
+        return jsonify({"status": 500,  "msg": "Please Enter From, Destination Language and Message in your API Resquest."})
+    translator = Translator(from_lang=data['from'], to_lang=data['to'])
     print("Posted Data", data['msg'])
     translated_msg = translator.translate(data['msg'])
     return jsonify({"msg": str(translated_msg)})
 
 
-app.run(port=5000)
+app.run(host="0.0.0.0", port=5000)
