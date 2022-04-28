caf957deaf69621a0bb9be2a012992a5223fd858
aman <er.amandeep04@gmail.com>
2022-04-28 17:45:54 +0530

    minor changes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from translate import Translator
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+@app.route("/translate", methods=['POST'])
+def translate():
+
+    data = request.get_json()
+    if 'to' not in data or 'msg' not in data is None:
+        return jsonify({"status": 500,  "msg": "Please Enter Destination Language and Message in your API Resquest."})
+    translator = Translator(to_lang=data['to'])
+    print("Posted Data", data['msg'])
+    translated_msg = translator.translate(data['msg'])
+    return jsonify({"msg": str(translated_msg)})
+
+
+app.run(port=5000)
